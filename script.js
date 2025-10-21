4b6525f9545dae2df102ffa072d42201a804fd8f
inmaria722 <tncrkak722@naver.com>
2025-10-21 20:48:55 +0900

    js, 수정

--- script.js
+++ script.js
@@ -19,10 +19,20 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 $(".title").each(function () {
-  var currentBox = $(this)[0];
+  var currentTitle = $(this)[0];
 
-  gsap.fromTo(
-    currentBox,
+  var currentSubs = $(this).nextAll(".sub").toArray();
+
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: currentTitle,
+      start: "top 80%",
+      toggleActions: "play none restart none",
+    },
+  });
+
+  tl.fromTo(
+    currentTitle,
     {
       opacity: 0,
       y: 50,
@@ -32,13 +42,23 @@ $(".title").each(function () {
       y: 0,
       duration: 0.6,
       ease: "power1.inout",
-
-      // 3. ScrollTrigger 설정
-      scrollTrigger: {
-        trigger: currentBox,
-        start: "top 80%",
-        toggleActions: "play none none none",
-      },
     }
   );
+
+  if (currentSubs.length > 0) {
+    tl.fromTo(
+      currentSubs,
+      {
+        opacity: 0,
+        y: 30,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        ease: "power1.inout",
+        stagger: 0.1,
+      }
+    );
+  }
 });
