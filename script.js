28f4e9c29a552e8eba580f71617127584fa9f2a4
inmaria722 <tncrkak722@naver.com>
2025-10-24 19:52:54 +0900

    js 수정

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
+$(document).ready(function () {
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+});
+
 gsap.registerPlugin(ScrollTrigger);
 
-const lenis = new Lenis();
-lenis.on("scroll", ScrollTrigger.update);
+$(window).on("load", function () {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 120,
+    centeredSlides: true,
+    initialSlide: 1,
+    slidesPerView: "auto",
+    speed: 550,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+  var $track = $(".marquee-track");
+  var $items = $track.children();
+  $items.clone().appendTo($track);
+  var marqueeTween = gsap.to($track, {
+    xPercent: -50,
+    duration: 10,
+    ease: "none",
+    repeat: -1,
+  });
+  $track.hover(
+    function () {
+      marqueeTween.pause();
+    },
+    function () {
+      marqueeTween.play();
+    }
+  );
 
-gsap.ticker.lagSmoothing(0);
+  const fashionZoomTween = gsap.to(".fashion-img img", {
+    scale: 1,
+    ease: "none",
+  });
 
-// swiper
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 120,
-  centeredSlides: true,
-  initialSlide: 1,
-  slidesPerView: "auto",
-  speed: 550,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: { el: ".swiper-pagination", clickable: true },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+  ScrollTrigger.create({
+    trigger: ".fashion-img",
+    animation: fashionZoomTween,
+    start: "top top",
+    end: "+=60%",
+    pin: true,
+    scrub: true,
+  });
 
-// fade up
-$(document).ready(function () {
-  gsap.registerPlugin(ScrollTrigger);
+  const elementsToAnimate = gsap.utils.toArray(
+    ".title-box > .title, .title-box > .sub, .img-box"
+  );
 
-  $(".title-box > .title, .title-box > .sub, .img-box").each(function () {
-    var currentElement = this;
-    var $el = $(currentElement);
-
-    ScrollTrigger.create({
-      trigger: currentElement,
-      start: "top 80%",
-      once: true,
-      onEnter: function () {
+  ScrollTrigger.batch(elementsToAnimate, {
+    start: "top 80%",
+    once: true,
+    anticipatePin: 1,
+    onEnter: (batch) => {
+      batch.forEach((el) => {
+        const $el = $(el);
         $el.addClass("is-visible");
 
         if ($el.hasClass("img-box")) {
@@ -47,45 +77,9 @@ $(document).ready(function () {
             $el.addClass("is-hoverable");
           }, 1000);
         }
-      },
-    });
-  });
-});
-
-// marquee
-$(document).ready(function () {
-  var $track = $(".marquee-track");
-  var $items = $track.children();
-  var itemWidth = 0;
-
-  $items.each(function () {
-    itemWidth += $(this).outerWidth(true);
-  });
-
-  $items.clone().appendTo($track);
-
-  function scrollMarquee() {
-    $track.animate(
-      {
-        marginLeft: -itemWidth,
-      },
-      45000,
-      "linear",
-      function () {
-        $(this).css({ marginLeft: 0 });
-        scrollMarquee();
-      }
-    );
-  }
-
-  scrollMarquee();
-
-  $track.hover(
-    function () {
-      $(this).stop();
+      });
     },
-    function () {
-      scrollMarquee();
-    }
-  );
+  });
+
+  ScrollTrigger.refresh();
 });
