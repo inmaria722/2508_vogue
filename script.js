48a0a82c1b3fbf6e832f7e1f163d0ddccaeaa270
inmaria722 <tncrkak722@naver.com>
2025-10-24 15:04:06 +0900

    marquee, js 추가

--- script.js
+++ script.js
@@ -19,40 +19,80 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 // fade up
-$(".title").each(function () {
-  var commonParent = $(this).closest(".con");
+$(document).ready(function () {
+  $(".title").each(function () {
+    var commonParent = $(this).closest(".con");
 
-  var currentTitle = $(this)[0];
-  var currentSubs = commonParent.find(".sub").toArray();
-  var currentImgBoxes = commonParent.find(".img-box").toArray();
+    var currentTitle = $(this)[0];
+    var currentSubs = commonParent.find(".sub").toArray();
+    var currentImgBoxes = commonParent.find(".img-box").toArray();
 
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: currentTitle,
-      start: "top 80%",
-      toggleActions: "play none none none",
-    },
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: currentTitle,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+    });
+
+    tl.fromTo(
+      currentTitle,
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 0.5, ease: "power1.inout" }
+    );
+
+    if (currentSubs.length > 0) {
+      tl.fromTo(
+        currentSubs,
+        { opacity: 0, y: 30 },
+        { opacity: 1, y: 0, duration: 0.5, ease: "power1.inout", stagger: 0.1 }
+      );
+    }
+
+    if (currentImgBoxes.length > 0) {
+      tl.fromTo(
+        currentImgBoxes,
+        { opacity: 0, y: 30 },
+        { opacity: 1, y: 0, duration: 0.5, ease: "power1.inout", stagger: 0.1 }
+      );
+    }
+  });
+});
+
+// marquee
+$(document).ready(function () {
+  var $track = $(".marquee-track");
+  var $items = $track.children();
+  var itemWidth = 0;
+
+  $items.each(function () {
+    itemWidth += $(this).outerWidth(true);
   });
 
-  tl.fromTo(
-    currentTitle,
-    { opacity: 0, y: 50 },
-    { opacity: 1, y: 0, duration: 0.6, ease: "power1.inout" }
+  $items.clone().appendTo($track);
+
+  function scrollMarquee() {
+    $track.animate(
+      {
+        marginLeft: -itemWidth,
+      },
+      45000,
+      "linear",
+      function () {
+        $(this).css({ marginLeft: 0 });
+        scrollMarquee();
+      }
+    );
+  }
+
+  scrollMarquee();
+
+  $track.hover(
+    function () {
+      $(this).stop();
+    },
+    function () {
+      scrollMarquee();
+    }
   );
-
-  if (currentSubs.length > 0) {
-    tl.fromTo(
-      currentSubs,
-      { opacity: 0, y: 30 },
-      { opacity: 1, y: 0, duration: 0.5, ease: "power1.inout", stagger: 0.1 }
-    );
-  }
-
-  if (currentImgBoxes.length > 0) {
-    tl.fromTo(
-      currentImgBoxes,
-      { opacity: 0, y: 30 },
-      { opacity: 1, y: 0, duration: 0.5, ease: "power1.inout", stagger: 0.1 }
-    );
-  }
 });
